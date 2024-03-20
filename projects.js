8c7fa3bcaf374fd9083d75d78cc86d29966c162c
Tatyana <156218366+Tatyanac94@users.noreply.github.com>
2024-03-20 22:24:11 +0000

    Created an array with a for loop inside a funtion to populate an unordered list when the button is clicked

---
+++ projects.js
@@ -0,0 +1,22 @@
+//I declared an array named skills to hold my current skills. I created a for loop inside a function to iterate through the array and display each skill in an unordered list. The SkillsEvent function is called when the button is clicked.
+
+let skills = ["HTML",
+                 "Javascript",
+                 "CSS",
+               "Bootstrap",
+               "Troubleshooting",
+                "Networking",
+                "Security",
+               "Hardware"]
+
+let skillContainer = document.getElementById("myULS");
+let skillsButton = document.getElementById("SB");
+skillsButton.addEventListener("click", SkillsEvent);
+function SkillsEvent(){
+  for (let i = 0; i < skills.length; i++){
+    let skill = document.createElement("li");
+    skill.classList.add("list-group-item");
+    skill.textContent = skills[i];
+   skillContainer.appendChild(skill);
+  }
+}
